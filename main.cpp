0bb790a7573b5bbe4c992e4f0e8dccccf9e35a1c
Thomas Weinhold <Thomas.Weinhold@stratoinos.de>
2018-01-04 06:11:44 +0100

    save/restore window settings

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include "mainwindow.h"
+#include <QCoreApplication>
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
+    QCoreApplication::setOrganizationName("Oinosseus");
+    QCoreApplication::setOrganizationDomain("private");
+    QCoreApplication::setApplicationName("SshfsMount");
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,6 +2,7 @@
 
 #include <QTreeWidgetItem>
 #include <QStringList>
+#include <QSettings>
 
 #include "mounttreewidget.h"
 #include "mountitemgroup.h"
@@ -12,8 +13,17 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
     // list view
     MountTreeWidget *mountTree = new MountTreeWidget();
     this->setCentralWidget(mountTree);
+
+    // restore window settings
+    QSettings settings;
+    this->restoreGeometry(settings.value("MainWindow/geometry").toByteArray());
+    this->restoreState(settings.value("MainWindow/windowState").toByteArray());
 }
 
 MainWindow::~MainWindow()
 {
+    // save window settings
+    QSettings settings;
+    settings.setValue("MainWindow/geometry", this->saveGeometry());
+    settings.setValue("MainWindow/windowState", this->saveState());
 }
