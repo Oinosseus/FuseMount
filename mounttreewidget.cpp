40330161c237279b7c8fda77c6998697f7750c96
Thomas Weinhold <Thomas.Weinhold@stratoinos.de>
2018-01-04 03:46:08 +0100

    sketching context menu items

--- mounttreewidget.cpp
+++ mounttreewidget.cpp
@@ -3,17 +3,36 @@
 #include <QMenu>
 #include <QAction>
 
+#include <mountitem.h>
+#include <mountitemgroup.h>
+
 MountTreeWidget::MountTreeWidget(QWidget *parent) : QTreeWidget(parent)
 {
     this->setHeaderLabels(QStringList() << "Name" << "mountpoint" << "server" << "state");
+    this->setEditTriggers(QAbstractItemView::DoubleClicked);
 }
 
 void MountTreeWidget::contextMenuEvent(QContextMenuEvent *event)
 {
+    QTreeWidgetItem *twi = this->currentItem();
+
     QMenu menu(this);
-    menu.addAction(new QAction(tr("add Group")));
-    menu.addAction(new QAction(tr("add Mount")));
+
+    // group administrative actions
+    menu.addAction(new QAction(tr("add new group")));
+    if (twi->type() == MOUNTITEMGROUP_TYPE) {
+        menu.addAction(new QAction(tr("delete group") + " " + twi->text(0)));
+    }
     menu.addSeparator();
+
+    // mount administrative actions
+    menu.addAction(new QAction(tr("add new mount")));
+    if (twi->type() == MOUNTITEM_TYPE) {
+        menu.addAction(new QAction(tr("delete mount") + " " + twi->text(0)));
+    }
+    menu.addSeparator();
+
+
     menu.addAction(new QAction("Foo2"));
     menu.exec(event->globalPos());
 }
