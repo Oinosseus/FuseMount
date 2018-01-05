145baae9e8af975be232ed2e65d1a6e10d771196
Thomas Weinhold <Thomas.Weinhold@stratoinos.de>
2018-01-05 01:40:06 +0100

    including KDE main application stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,48 @@
 #include "mainwindow.h"
+
 #include <QCoreApplication>
 #include <QApplication>
+#include <QCommandLineParser>
+
+#include <KAboutData>
+#include <KLocalizedString>
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication::setOrganizationName("Oinosseus");
-    QCoreApplication::setOrganizationDomain("private");
-    QCoreApplication::setApplicationName("SshfsMount");
-
     QApplication a(argc, argv);
+    KLocalizedString::setApplicationDomain("sshfsmount");
+
+    KAboutData aboutData(
+                            // The program name used internally. (componentName)
+                            QStringLiteral("sshfsmount"),
+                            // A displayable program name string. (displayName)
+                            i18n("SshfsMount"),
+                            // The program version string. (version)
+                            QStringLiteral("0.1a"),
+                            // Short description of what the app does. (shortDescription)
+                            i18n("GUI frontend for sshfs."),
+                            // The license this code is released under
+                            KAboutLicense::GPL,
+                            // Copyright Statement (copyrightStatement = QString())
+                            i18n("(c) 2018"),
+                            // Optional text shown in the About box.
+                            // Can contain any information desired. (otherText)
+                            i18n("Frontend for sshfs allowing grouped mounts and using KWallet."),
+                            // The program homepage string. (homePageAddress = QString())
+                            QStringLiteral("http://www.example.com"),
+                            // The bug report email address
+                            // (bugsEmailAddress = QLatin1String("submit@bugs.kde.org")
+                            QStringLiteral(""));
+    aboutData.addAuthor(i18n("Thomas Weinhold"), i18n(""), QStringLiteral("Thomas.Weinhold@stratoinos.de"), QStringLiteral(""), QStringLiteral(""));
+    KAboutData::setApplicationData(aboutData);
+
+    QCommandLineParser parser;
+    parser.addHelpOption();
+    parser.addVersionOption();
+    aboutData.setupCommandLine(&parser);
+    parser.process(a);
+    aboutData.processCommandLine(&parser);
+
     MainWindow w;
     w.show();
 
